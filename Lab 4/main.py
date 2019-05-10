053537e667d1220ee60ec1fcdffea068b3e2e494
NasterVill <skatrr@gmail.com>
2019-05-10 11:51:14 +0300

    Lab 4 was implemented

--- Lab 4/main.py
+++ Lab 4/main.py
@@ -1,4 +1,5 @@
 import transform as tr
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -8,13 +9,49 @@ def main():
     synthesis_low_pass = [1, 1]
     synthesis_high_pass = [1, -1]
     initial_data = [10, 6, 6, 1, 7, 2, 13, 4, 12, 10, 11, 8, 6, 10, 12, 13]
+    initial_len = len(initial_data)
+    time_stems = [i for i in range(1, len(initial_data) + 1)]
 
-    wavelet_transform_result, decomposition_level = \
-        tr.fwt(initial_data, analysis_low_pass, analysis_high_pass, max_decomposition_depth)
+    wavelet_transform_result = tr.temp_fwt(initial_data, max_decomposition_depth)
+    inverse_wavelet_results = []
+    for i in range(max_decomposition_depth):
+        inverse_wavelet_results.append(tr.temp_ifwt(wavelet_transform_result, i + 1))
 
-    print(wavelet_transform_result, decomposition_level)
+    for i in range(max_decomposition_depth):
+        extended_result = []
+        for x in inverse_wavelet_results[i]:
+            extended_result += [x for i in range(int(initial_len / len(inverse_wavelet_results[i])))]
 
-    # print(tr.linear_convolution([1,1,5,5], [3,4]))
+        inverse_wavelet_results[i] = extended_result
+        print('e', extended_result)
+
+    print(wavelet_transform_result)
+
+    fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2)
+
+    ax1.plot(time_stems, initial_data)
+    ax1.set(title='Initial signal')
+    ax1.grid()
+
+    ax3.plot(time_stems, inverse_wavelet_results[3])
+    ax3.set(title='IFWT level 4')
+    ax3.grid()
+
+    ax4.plot(time_stems, inverse_wavelet_results[2])
+    ax4.set(title='IFWT level 3')
+    ax4.grid()
+
+    ax5.plot(time_stems, inverse_wavelet_results[1])
+    ax5.set(title='IFWT level 2')
+    ax5.grid()
+
+    ax6.plot(time_stems, inverse_wavelet_results[0])
+    ax6.set(title='IFWT level 1')
+    ax6.grid()
+
+    fig.delaxes(ax2)
+
+    plt.show()
 
 
 if __name__ == '__main__':
--- Lab 4/transform.py
+++ Lab 4/transform.py
@@ -1,26 +1,18 @@
 import numpy as np
 
 
-def linear_convolution(first_sequence: list, second_sequence: list, full: bool = False):
+def linear_convolution(first_sequence: list, second_sequence: list,):
     second_length = len(second_sequence)
 
-    if full:
-        processed_sequence = [0] * (second_length - 1) + first_sequence[:]
-        processed_sequence_length = len(processed_sequence)
-    else:
-        processed_sequence = first_sequence[:]
-        processed_sequence_length = len(processed_sequence) - 1
+    processed_sequence = [0] * (second_length - 1) + first_sequence[:]
+    processed_sequence_length = len(processed_sequence)
 
     result = []
     for i in range(processed_sequence_length):
         temp = 0
         for j in range(second_length):
-            if full:
-                k = (i + j) % processed_sequence_length
-                temp += processed_sequence[k] * second_sequence[j]
-            else:
-                if i + j < processed_sequence_length + 1:
-                    temp += processed_sequence[i + j] * second_sequence[j]
+            k = (i + j) % processed_sequence_length
+            temp += processed_sequence[k] * second_sequence[j]
 
         result.append(temp)
 
@@ -37,38 +29,84 @@ def fwt(signal: list, low_pass: list, high_pass: list, max_decomposition_depth: 
     return __fwt(signal, low_pass, high_pass, decomposition_depth), decomposition_depth
 
 
-def __fwt(signal: list, low_pass: list, high_pass: list, max_decomposition: int):
-    if len(signal) == 1 or max_decomposition == 0:
+def __fwt(signal: list, low_pass: list, high_pass: list, decomposition_depth: int):
+    if len(signal) == 1 or decomposition_depth == 0:
         return signal
 
-    approximation = decimate(linear_convolution(signal, low_pass))
-    detail = decimate(linear_convolution(signal, high_pass))
+    approximation = decimate(wkeep(linear_convolution(signal, low_pass), len(signal)))
+    detail = decimate(wkeep(linear_convolution(signal, high_pass), len(signal)))
 
     result = detail.copy()
 
-    result += __fwt(approximation, low_pass, high_pass, max_decomposition - 1)
+    result += __fwt(approximation, low_pass, high_pass, decomposition_depth - 1)
 
     return result
 
 
 def interpolate(sequence: list):
-    return [sequence[i] if i % 2 == 0 else 0 for i in range(len(sequence) * 2)]
+    return [sequence[int(i / 2)] if i % 2 == 0 else 0 for i in range(len(sequence) * 2)]
 
 
 def ifwt(wavelet_coefficients: list, low_pass: list, high_pass: list, decomposition_depth: int):
+    temp_wavelet_coefficients = wavelet_coefficients[:]
+    temp_wavelet_coefficients.reverse()
+
+    return __ifwt(wavelet_coefficients, low_pass, high_pass, decomposition_depth)
 
 
-# def ifwt(wavelet_coefficients: list, low_pass: list, high_pass: list, decomposition_depth: int):
-#     temp_wavelet_coefficients = wavelet_coefficients[:]
-#     temp_wavelet_coefficients.reverse()
-#
-#     return __ifwt(wavelet_coefficients, low_pass, high_pass, decomposition_depth)
+def wkeep(x: list, y: int):
+    if len(x) <= y:
+        return x
+
+    diff = len(x) - y
+    if diff % 2 == 0:
+        return x[int(diff / 2):-int(diff / 2):]
+    else:
+        return x[int(diff / 2):-(int(diff / 2) + 1):]
 
 
-# def __ifwt(wavelet_coefficients: list, low_pass: list, high_pass: list, decomposition_depth: int):
-#     higher_level_approximation = linear_convolution(interpolate(wavelet_coefficients), low_pass)
-#     higher_level_detail = linear_convolution(interpolate(wavelet_coefficients), high_pass)
-#
-#     result = [higher_level_approximation[i] + higher_level_detail[i] for i in range(len(higher_level_approximation))]
-#
-#     return __ifwt(result, low_pass, high_pass, decomposition_depth)
+def __ifwt(wavelet_coefficients: list, low_pass: list, high_pass: list, decomposition_depth: int):
+    if len(wavelet_coefficients) == 1 or decomposition_depth == 0:
+        return wavelet_coefficients
+
+    length = len(wavelet_coefficients)
+    halfed_length = int(length / 2)
+
+    detail = wavelet_coefficients[halfed_length:]
+    approximation = __ifwt(wavelet_coefficients[:halfed_length], low_pass, high_pass, decomposition_depth - 1)
+
+    detail = wkeep(linear_convolution(interpolate(detail), high_pass), length)
+    approximation = wkeep(linear_convolution(interpolate(approximation), low_pass), length)
+
+    return [x + y for x, y in zip(approximation, detail)]
+
+
+def temp_fwt(signal: list, decomposition_depth: int):
+    if len(signal) == 1 or decomposition_depth == 0:
+        return signal
+
+    detail = [(signal[i] - signal[i + 1]) / 2 for i in range(0, len(signal), 2)]
+    approximation = [(signal[i] + signal[i + 1]) / 2 for i in range(0, len(signal), 2)]
+
+    result = detail + temp_fwt(approximation, decomposition_depth - 1)
+
+    return result
+
+
+def temp_ifwt(wavelet_coefficients: list, decomposition_depth: int):
+    if len(wavelet_coefficients) == 1:
+        return wavelet_coefficients
+
+    detail = [x for x in wavelet_coefficients[int(len(wavelet_coefficients) / 2)::]]
+
+    approximation = temp_ifwt(detail, decomposition_depth - 1)
+
+    if decomposition_depth > 1:
+        return approximation
+
+    result = []
+    for i in range(int(len(wavelet_coefficients) / 2)):
+        result.append(approximation[i] + wavelet_coefficients[i])
+        result.append(approximation[i] - wavelet_coefficients[i])
+
+    return result
