7428a225f6d2939e62a49991070a299eb510296c
NasterVill <skatrr@gmail.com>
2019-05-10 12:15:54 +0300

    Temporary code saving

--- Lab 4/main.py
+++ Lab 4/main.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 
 def main():
     max_decomposition_depth = 4
+    analysis_low_pass = [0.5, 0.5]
+    analysis_high_pass = [0.5, -0.5]
+    synthesis_low_pass = [1, 1]
+    synthesis_high_pass = [1, -1]
     initial_data = [10, 6, 6, 1, 7, 2, 13, 4, 12, 10, 11, 8, 6, 10, 12, 13]
     initial_len = len(initial_data)
     time_stems = [i for i in range(1, len(initial_data) + 1)]
